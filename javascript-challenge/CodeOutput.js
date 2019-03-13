d031603e5e93dfc2e1c95d5bed1c1334916d8c13
Sergei Stadnik <sergeisqa@gmail.com>
2019-03-13 13:50:35 -0700

    Added CodeOutput

---
+++ javascript-challenge/CodeOutput.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * What is the output of the code?
+ * */
+var a =10;
+var z=- (a % 2);
+document.write(z);
+
+// returns 0
+
+/*
+Let`s look at the code.
+At first, a = 10.
+Then we have to calculate the modulos of 10 % 2 = 0 . 
+Minus zero is still 0, so the answer is 0.
+
+Find the simple  modulo calculator below
+https://www.miniwebtool.com/modulo-calculator/.
+*/
